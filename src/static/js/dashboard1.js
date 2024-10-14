f616b9f40204e3c20498667baa9c3a99bec3bbed
Daniella Simara <150517240+dmsimara@users.noreply.github.com>
2024-10-15 00:18:31 +0800

    added tenant popup

--- src/static/js/dashboard1.js
+++ src/static/js/dashboard1.js
@@ -1,11 +1,8 @@
-// for styles to be applied not just with hover
-// Get all the tab links
+// for styles to be applied not just with hover - tabs header
 const tabs = document.querySelectorAll('.tabs ul li a');
 
-// Get the current URL path (only the file name)
 const currentPath = window.location.pathname.split("/").pop();
 
-// Loop through each tab and add 'active-tab' to the corresponding tab based on URL
 tabs.forEach(tab => {
     const tabPath = tab.getAttribute('href').split("/").pop();
     if (tabPath === currentPath) {
@@ -14,34 +11,10 @@ tabs.forEach(tab => {
 });
 
 
-
-// for see more dropdown functionality
-const dropdowns = document.querySelectorAll('.dropdown');
-
-dropdowns.forEach(dropdown => {
-    const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelectorAll('.menu li');
-    const selected = dropdown.querySelector('.selected');
-
-    select.addEventListener('click', () => {
-        select.classList.toggle('select-clicked');
-        caret.classList.toggle('caret-rotate');
-        menu.classList.toggle('menu-open'); 
-    });
-
-    options.forEach(option => {
-        option.addEventListener('click', () => {
-            selected.innerText = option.innerText;
-            select.classList.remove('select-clicked');
-            caret.classList.remove('caret-rotate');
-            menu.classList.remove('menu-open');
-
-            options.forEach(opt => {
-                opt.classList.remove('active');
-            });
-            option.classList.add('active');
-        });
-    });
-});
+// for tenants info pop-up
+function toggle() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active'); // Toggles blur effect
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active'); // Toggles popup visibility
+}
