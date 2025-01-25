42b01ace05bb12b398084c726041250158e4c3c4
Daniella Simara <150517240+dmsimara@users.noreply.github.com>
2025-01-25 06:16:02 -0800

    Update introduction.js

--- client/public/js/introduction.js
+++ client/public/js/introduction.js
@@ -1,16 +1,12 @@
-// document.querySelector('.get-hive-free').addEventListener('click', function(e) {
-//     e.preventDefault();
-//     this.innerText = 'Downloading...';
-//     setTimeout(() => {
-//         window.location.href = '#';
-//     }, 1000); 
-// });
-
 document.querySelector('.get-hive-free').addEventListener('click', function (e) {
     e.preventDefault();  
-    this.innerText = 'Downloading...';  
+
+    this.innerText = 'Downloading...'; 
+
     setTimeout(() => {
-        alert('Your Hive app download is starting now. If the download doesn’t begin, please try again.');  
-        this.innerText = 'Download App';  
-    }, 1000);
+        alert('Your Hive app download is starting now. If the download doesn’t begin, please try again.');
+        this.innerText = 'Get Hive Free';
+
+        window.location.href = 'https://drive.google.com/uc?export=download&id=1QjCzqeozzzO9ztjJthNrXhsof-ot0eWV';
+    }, 1000); 
 });
