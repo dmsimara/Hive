4f51a5f4e8f86a1c512331849826ee7d3b64a817
Daniella Simara <150517240+dmsimara@users.noreply.github.com>
2025-01-25 06:21:32 -0800

    Update introduction.js

--- client/public/js/introduction.js
+++ client/public/js/introduction.js
@@ -4,7 +4,7 @@ document.querySelector('.get-hive-free').addEventListener('click', function (e) 
     this.innerText = 'Downloading...'; 
 
     setTimeout(() => {
-        alert('Your Hive app download is starting now. If the download doesn’t begin, please try again.');
+        alert('Google Drive may show a warning because this file is large and executable. Click "Download Anyway" to continue.');
         this.innerText = 'Get Hive Free';
 
         window.location.href = 'https://drive.google.com/uc?export=download&id=1QjCzqeozzzO9ztjJthNrXhsof-ot0eWV';
